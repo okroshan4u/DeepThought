c21cd40a3f60636e8c9393a4202147ade0c14ac5
Roshan <155551955+okroshan4u@users.noreply.github.com>
2025-12-11 18:36:59 +0530

    Fix formatting issue in server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const eventRoutes = require("./routes/eventRoutes");
 const app = express();
 
 app.use(express.json());
-app.use("/api/v3/app", eventRoutes); // base URL
+app.use("/api/v3/app", eventRoutes); 
 
 const PORT = 5000;
 
